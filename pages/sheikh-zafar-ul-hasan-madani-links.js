86c7d829d46349bb9214fbb6e37195e1b1bc8e19
sheikh-zafar <audios.shkzafar@gmail.com>
2021-12-14 07:58:48 +0530

    commit

--- pages/sheikh-zafar-ul-hasan-madani-links.js
+++ pages/sheikh-zafar-ul-hasan-madani-links.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Navbar from "./Navbar";
+import Navbar from "./nav";
 import Footer from "./footer";
 import Logo from "./img/logo192.png";
 import Image from "next/image";
